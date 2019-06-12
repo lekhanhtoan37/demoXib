99d0782d5a5e246c70aedb9c92081c88bf37b7ac
Lê Toàn <toanlk@paditech.org>
2019-06-12 10:48:36 +0700

    xib

---
+++ DemoXib/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  DemoXib
+//
+//  Created by Lê Toàn on 6/12/19.
+//  Copyright © 2019 Lê Toàn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var content: UIView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+//    view.backgroundColor = .red/
+    
+  }
+//  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+//    super.init(nibName: "ViewController", bundle: nil)
+//  }
+//
+//  required init?(coder aDecoder: NSCoder) {
+//    fatalError("init(coder:) has not been implemented")
+//  }
+  
+}
+
